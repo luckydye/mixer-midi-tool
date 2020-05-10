ff2bad5d53eb0bd716785f081b3ee334ab1c8d34
Tim Havlicek <tim.h4vlicek@gmail.com>
2020-05-10 09:24:55 +0200

    fix config

--- tool/Config.py
+++ tool/Config.py
@@ -1,16 +1,20 @@
 import json
+import os 
+
+def get_path():
+    return os.path.dirname(os.path.realpath(__file__))
 
 class Config:
 
     datastore = None
 
     def save(self):
-        json_file  = open("tool/config.json", "w") 
+        json_file  = open(get_path() + "\config.json", "w") 
         json_string = json.dumps(self.datastore, indent=4)
         json_file.write(json_string)
 
     def load(self):
-        json_file  = open("tool/config.json", "r") 
+        json_file  = open(get_path() + "\config.json", "r") 
         json_string = json_file.read()
         self.datastore = json.loads(json_string)
         print(self.datastore)
